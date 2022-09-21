72d94a2911e4e69b9efc7102396059b3a50ee15c
Christian LJ C. Raytos <christianraytos01205@gmail.com>
2022-09-21 18:16:57 +0800

    add image component

--- lib/literature/views/blog_view.ex
+++ lib/literature/views/blog_view.ex
@@ -1,7 +1,9 @@
 defmodule Literature.BlogView do
   use Literature.Web, :view
 
+  import Literature.ImageComponent
   import Literature.LayoutView, only: [asset_path: 2]
+
   alias Literature.Config
 
   def favicon_path(conn), do: asset_path(conn, "favicon/favicon.ico")
@@ -9,6 +11,5 @@ defmodule Literature.BlogView do
   def js_path(conn), do: asset_path(conn, "js/app.js")
 
   defp logo, do: Config.logo()
-
   defp title, do: Config.title()
 end
