a4bbf8724aea82ca7ccbaeba25ddf04778475c30
Christian LJ C. Raytos <christianraytos01205@gmail.com>
2022-10-16 22:47:29 +0800

    update uploader storage_dir

--- lib/literature/uploaders.ex
+++ lib/literature/uploaders.ex
@@ -12,7 +12,7 @@ defmodule Literature.Uploaders do
   alias Literature.Config
 
   @extension_whitelist ~w(.jpg .jpeg .png)
-  @versions ~w(original jpg webp avif)a
+  @versions ~w(original jpg webp)a
 
   def asset_host, do: Config.waffle_asset_host()
   def bucket, do: Config.waffle_bucket()
@@ -40,7 +40,21 @@ defmodule Literature.Uploaders do
     {:convert, "-format avif", :avif}
   end
 
-  def storage_dir(_, _), do: "literature"
+  def storage_dir(:original, {file, _}) do
+    file_name = Path.basename(file.file_name, Path.extname(file.file_name))
+
+    "literature/#{file_name}"
+  end
+
+  def storage_dir(version, {file, _}) do
+    file_name =
+      file.file_name
+      |> Path.basename(Path.extname(file.file_name))
+      |> String.split("-")
+      |> List.delete_at(-1)
+
+    "literature/#{file_name}/#{version}"
+  end
 
   def filename(version, {file, _}, size) do
     file_name = Path.basename(file.file_name, Path.extname(file.file_name))
