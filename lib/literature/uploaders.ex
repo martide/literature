afb446f67b4be3f83850e6586bea10653cd30e35
Sam Hamilton <sam@sh81.com>
2022-10-18 14:35:27 +0800

    fix extenions whitelist

--- lib/literature/uploaders.ex
+++ lib/literature/uploaders.ex
@@ -11,7 +11,7 @@ defmodule Literature.Uploaders do
 
   alias Literature.Config
 
-  @extension_whitelist ~w(.png)
+  @extension_whitelist ~w(.jpg .jpeg .png)
   @versions ~w(original jpg webp avif)a
 
   def asset_host, do: Config.waffle_asset_host()
