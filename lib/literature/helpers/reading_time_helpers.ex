f21dfef0d919fdafa0106686f05948a98749402f
Christian LJ C. Raytos <52639567+christianlj@users.noreply.github.com>
2022-10-21 14:18:48 +0800

    change hard-coded (#21)

---
+++ lib/literature/helpers/reading_time_helpers.ex
@@ -0,0 +1,28 @@
+defmodule Literature.ReadingTimeHelpers do
+  @moduledoc false
+
+  @doc """
+  Count the words of a setence
+  """
+  def reading_time(html, words_per_minute \\ 150) do
+    html
+    |> Enum.join("")
+    |> String.split(~r/[\s_&!?:@$%^,\.]+/)
+    |> Enum.count()
+    |> estimate(words_per_minute)
+    |> output()
+  end
+
+  defp estimate(words, words_per_minute),
+    do: Float.ceil(words / words_per_minute) |> trunc
+
+  defp output(reading_time) do
+    cond do
+      reading_time <= 1 ->
+        "1 min read"
+
+      reading_time > 1 ->
+        "#{reading_time} mins read"
+    end
+  end
+end
