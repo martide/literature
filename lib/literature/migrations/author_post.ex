a947f58c4d400c29e4a5f4b1aa35e695e7a4cd55
jeffmartide <jeffrey@martide.com>
2023-04-14 14:25:01 +0800

    [WEB-7737] Add more indexes to literature

--- lib/literature/migrations/author_post.ex
+++ lib/literature/migrations/author_post.ex
@@ -1,15 +1,25 @@
 defmodule Literature.Migrations.AuthorPost do
   @moduledoc false
   use Ecto.Migration
+  @disable_ddl_transaction true
+  @disable_migration_lock true
 
   def up do
     create_if_not_exists table(:literature_authors_posts, primary_key: false) do
       add(:author_id, references(:literature_authors, on_delete: :delete_all, type: :binary_id))
       add(:post_id, references(:literature_posts, on_delete: :delete_all, type: :binary_id))
     end
+
+    create(
+      unique_index("literature_authors_posts", [:post_id, :author_id], concurrently: true)
+    )
   end
 
   def down do
     drop_if_exists(table(:literature_authors_posts))
+
+    drop_if_exists(
+      index("literature_authors_posts", [:post_id, :author_id], concurrently: true)
+    )
   end
 end
--- lib/literature/migrations/tag_post.ex
+++ lib/literature/migrations/tag_post.ex
@@ -1,15 +1,25 @@
 defmodule Literature.Migrations.TagPost do
   @moduledoc false
   use Ecto.Migration
+  @disable_ddl_transaction true
+  @disable_migration_lock true
 
   def up do
     create_if_not_exists table(:literature_tags_posts, primary_key: false) do
       add(:tag_id, references(:literature_tags, on_delete: :delete_all, type: :binary_id))
       add(:post_id, references(:literature_posts, on_delete: :delete_all, type: :binary_id))
     end
+
+    create(
+      unique_index("literature_tags_posts", [:post_id, :tag_id], concurrently: true)
+    )
   end
 
   def down do
     drop_if_exists(table(:literature_tags_posts))
+
+    drop_if_exists(
+      index("literature_tags_posts", [:post_id, :tag_id], concurrently: true)
+    )
   end
 end
