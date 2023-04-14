717748c706b71deaa1c1ab530886054aed7b7378
jeffmartide <jeffrey@martide.com>
2023-04-14 22:02:17 +0800

    [WEB-7737] Migration with versioning

---
+++ lib/literature/migrations/postgres/v02.ex
@@ -0,0 +1,18 @@
+defmodule Literature.Migrations.Postgres.V02 do
+  @moduledoc """
+    Adds more indexes
+  """
+  use Ecto.Migration
+
+  def up(_opts) do
+    create_if_not_exists(unique_index("literature_authors_posts", [:post_id, :author_id], concurrently: true))
+    create_if_not_exists(unique_index("literature_posts", [:publication_id, :published_at], concurrently: true))
+    create_if_not_exists(unique_index("literature_tags_posts", [:post_id, :tag_id], concurrently: true))
+  end
+
+  def down(_opts) do
+    drop_if_exists(index("literature_authors_posts", [:post_id, :author_id], concurrently: true))
+    drop_if_exists(index("literature_posts", [:publication_id, :published_at], concurrently: true))
+    drop_if_exists(index("literature_tags_posts", [:post_id, :tag_id], concurrently: true))
+  end
+end
