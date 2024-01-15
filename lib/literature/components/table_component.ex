b7630326c75fa20450bf68a4420816c57e717a7b
Rommel Monsayac <rommelmonsayac@gmail.com>
2024-01-15 11:20:25 +0800

    Remove unused

--- lib/literature/components/table_component.ex
+++ lib/literature/components/table_component.ex
@@ -387,36 +387,4 @@ defmodule Literature.TableComponent do
 
   defp reverse("desc"), do: "asc"
   defp reverse(_), do: "desc"
-
-  def chevron_left(assigns) do
-    ~H"""
-    <svg
-      class="h-5 w-5"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="2"
-      stroke="currentColor"
-      aria-hidden="true"
-    >
-      <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
-    </svg>
-    """
-  end
-
-  def chevron_right(assigns) do
-    ~H"""
-    <svg
-      class="h-5 w-5"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="2"
-      stroke="currentColor"
-      aria-hidden="true"
-    >
-      <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-    </svg>
-    """
-  end
 end
