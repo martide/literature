5b2ef8668c6865b2265b76cbd794055ff377837d
Rommel Monsayac <rommelmonsayac@gmail.com>
2024-01-22 22:17:26 +0800

    Fix alt text cut off after single quote

--- mix.exs
+++ mix.exs
@@ -48,8 +48,8 @@ defmodule Literature.MixProject do
       {:phoenix, "~> 1.7"},
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_live_view, "~> 0.18"},
+      {:phoenix_html, "~> 3.3"},
       {:phoenix_view, "~> 2.0"},
-      {:phoenix_html_helpers, "~> 1.0"},
       {:postgrex, ">= 0.0.0", only: :test},
 
       # Pagination
