eed8cc9e0e81d83b0336ac4cb42a8b1bdaa4ed44
Rommel Monsayac <rommelmonsayac@gmail.com>
2024-10-14 19:03:34 +0800

    Use only impl for Ecto query

--- test/literature/pagination_test.exs
+++ test/literature/pagination_test.exs
@@ -2,191 +2,15 @@ defmodule Literature.PaginationTest do
   use Literature.DataCase
 
   import Literature.Test.Fixtures
-  import Ecto.Query, only: [from: 2, where: 2]
 
   alias Literature.Pagination
   alias Literature.Post
 
-  @languages [
-    "C#",
-    "C++",
-    "Clojure",
-    "Elixir",
-    "Erlang",
-    "Go",
-    "JAVA",
-    "JavaScript",
-    "Lisp",
-    "PHP",
-    "Perl",
-    "Python",
-    "Ruby",
-    "Rust",
-    "SQL"
-  ]
-  @total_entries length(@languages)
   @pagination_params %{page: 2, page_size: 4}
 
-  describe "paginate list using a repo module" do
-    setup do
-      Application.put_env(:flop, :default_limit, 5)
-    end
+  setup :setup_posts
 
-    test "can paginate a list using a Repo module  - 1" do
-      page = Repo.paginate(@languages)
-
-      assert page.page_size == 5
-      assert page.page_number == 1
-      assert page.entries == ["C#", "C++", "Clojure", "Elixir", "Erlang"]
-      assert page.total_entries == @total_entries
-      assert page.total_pages == Float.ceil(length(@languages) / 5)
-    end
-
-    test "can paginate a list using a Repo module - 2" do
-      languages = Enum.take(@languages, 3)
-      page = Repo.paginate(languages)
-
-      assert page.page_number == 1
-      assert page.page_size == 5
-      assert page.entries == ["C#", "C++", "Clojure"]
-      assert page.total_entries == 3
-      assert page.total_pages == Float.ceil(length(languages) / 5)
-    end
-
-    test "can paginate a list when provided the current page and page size as a params map" do
-      page = Repo.paginate(@languages, %{"page" => "2", "page_size" => "3"})
-
-      assert page.page_size == 3
-      assert page.page_number == 2
-      assert page.entries == ["Elixir", "Erlang", "Go"]
-      assert page.total_entries == @total_entries
-      assert page.total_pages == Float.ceil(length(@languages) / 3)
-    end
-
-    test "can paginate a list when only provided with the page size as a keyword list
-        and the page number defaults to page 1" do
-      page = Repo.paginate(@languages, page_size: 3)
-
-      assert page.page_size == 3
-      assert page.page_number == 1
-      assert page.entries == ["C#", "C++", "Clojure"]
-      assert page.total_pages == Float.ceil(length(@languages) / 3)
-      assert page.total_entries == @total_entries
-    end
-
-    test "can paginate a list when only provided with the page number as a keyword list
-        and the page size defaults to the Repo configured page size" do
-      page = Repo.paginate(@languages, page: 2)
-
-      assert page.page_size == 5
-      assert page.page_number == 2
-      assert page.entries == ["Go", "JAVA", "JavaScript", "Lisp", "PHP"]
-      assert page.total_pages == Float.ceil(length(@languages) / 5)
-      assert page.total_entries == @total_entries
-    end
-
-    test "can respect a max_limit configuration" do
-      Application.put_env(:flop, :max_limit, 10)
-
-      assert_raise Flop.InvalidParamsError, fn ->
-        Repo.paginate(@languages, page: 2, page_size: 200)
-      end
-    end
-  end
-
-  describe "paginate list without using a repo module" do
-    test "can paginate a list when passed a pagination params directly - 1" do
-      page = Pagination.paginate(@languages, @pagination_params, [])
-
-      assert page.page_number == 2
-      assert page.page_size == 4
-      assert page.entries == ["Erlang", "Go", "JAVA", "JavaScript"]
-      assert page.total_entries == @total_entries
-      assert page.total_pages == Float.ceil(length(@languages) / 4)
-    end
-
-    test "can paginate a list when passed a pagination params directly - 2" do
-      languages = Enum.take(@languages, 3)
-      page = Pagination.paginate(languages, @pagination_params, [])
-
-      assert page.page_number == 2
-      assert page.page_size == 4
-      assert page.entries == []
-      assert page.total_entries == 3
-      assert page.total_pages == Float.ceil(length(languages) / 4)
-    end
-
-    test "raises an error when provided the current page and page size as a params map" do
-      assert_raise ArithmeticError, fn ->
-        Pagination.paginate(@languages, %{"page" => "2", "page_size" => "3"}, [])
-      end
-    end
-
-    test "can paginate a list when provided the current page and page size as a keyword list" do
-      page = Pagination.paginate(@languages, [page: 2, page_size: 3], [])
-
-      assert page.page_size == 3
-      assert page.page_number == 2
-      assert page.entries == ["Elixir", "Erlang", "Go"]
-      assert page.total_pages == Float.ceil(length(@languages) / 3)
-      assert page.total_entries == @total_entries
-    end
-
-    test "raises an error when only provided with the page size as a keyword list" do
-      assert_raise ArithmeticError, fn ->
-        Pagination.paginate(@languages, [page_size: 3], [])
-      end
-    end
-
-    test "raises an error when only provided with the page number as a keyword list" do
-      assert_raise ArithmeticError, fn ->
-        Pagination.paginate(@languages, [page: 2], [])
-      end
-    end
-
-    test "cannot respect a max_limit configuration" do
-      Application.put_env(:flop, :max_limit, 10)
-      page = Pagination.paginate(@languages, [page: 2, page_size: 200], [])
-
-      refute page.page_size < 200
-      assert page.page_number == 2
-      assert page.entries == []
-      assert page.total_pages == Float.ceil(length(@languages) / 200)
-      assert page.total_entries == @total_entries
-    end
-  end
-
-  describe "paginate Literature resource / and Ecto query in using repo module" do
-    setup do
-      Application.put_env(:flop, :default_limit, 5)
-
-      publication =
-        publication_fixture(
-          name: "Blog",
-          description: "Blog description",
-          locale: "en",
-          ex_default_locale: "en"
-        )
-
-      author =
-        author_fixture(publication_id: publication.id, name: "Author", bio: "Author description")
-
-      tag =
-        tag_fixture(publication_id: publication.id, name: "Tag", description: "Tag description")
-
-      posts =
-        for i <- 1..20 do
-          post_fixture(
-            title: "Post #{i}",
-            publication_id: publication.id,
-            authors_ids: [author.id],
-            tags_ids: [tag.id]
-          )
-        end
-
-      {:ok, binding()}
-    end
-
+  describe "paginate Literature resource / and Ecto query using repo module" do
     test "can paginate resource using a Repo module", %{posts: posts} do
       page = Repo.paginate(Post)
 
@@ -227,6 +51,14 @@ defmodule Literature.PaginationTest do
       assert page.total_pages == Float.ceil(length(posts) / 5)
     end
 
+    test "can respect a max_limit configuration", %{posts: posts} do
+      Application.put_env(:flop, :max_limit, 10)
+
+      assert_raise Flop.InvalidParamsError, fn ->
+        Repo.paginate(posts, page: 2, page_size: 200)
+      end
+    end
+
     test "can paginate with ecto query provided", %{publication: publication} do
       publication_2 = publication_fixture()
       author_2 = author_fixture(publication_id: publication.id)
@@ -254,5 +86,89 @@ defmodule Literature.PaginationTest do
       assert page.total_entries == Enum.count(posts)
       assert page.total_pages == Float.ceil(length(posts) / 5)
     end
+  end
+
+  describe "paginate Literature resource and Ecto query without repo module" do
+    test "can paginate when passed with a pagination params directly", %{posts: posts} do
+      page = Pagination.paginate(Post, @pagination_params, [])
+
+      assert page.page_size == 4
+      assert page.page_number == 2
+      assert page.total_entries == Enum.count(posts)
+      assert page.total_pages == Float.ceil(length(posts) / 4)
+    end
+
+    test "can paginate when passed with string pagination params", %{posts: posts} do
+      page = Pagination.paginate(Post, %{"page" => "2", "page_size" => "5"}, [])
+
+      assert page.page_size == 5
+      assert page.page_number == 2
+      assert page.total_entries == Enum.count(posts)
+      assert page.total_pages == Float.ceil(length(posts) / 5)
+    end
+
+    test "can respect a max_limit configuration" do
+      Application.put_env(:flop, :max_limit, 10)
+
+      assert_raise Flop.InvalidParamsError, fn ->
+        Pagination.paginate(Post, %{page: 2, page_size: 200}, [])
+      end
+    end
+
+    test "can paginate with ecto query provided", %{publication: publication} do
+      publication_2 = publication_fixture()
+      author_2 = author_fixture(publication_id: publication.id)
+      tag_2 = tag_fixture(publication_id: publication.id)
+
+      posts =
+        for i <- 1..20 do
+          post_fixture(
+            title: "Post 2 #{i}",
+            publication_id: publication_2.id,
+            authors_ids: [author_2.id],
+            tags_ids: [tag_2.id]
+          )
+        end
+
+      query =
+        Post
+        |> from(as: :post)
+        |> where(publication_id: ^publication_2.id)
+
+      page = Pagination.paginate(query, @pagination_params, [])
+
+      assert page.page_size == 4
+      assert page.page_number == 2
+      assert page.total_entries == Enum.count(posts)
+      assert page.total_pages == Float.ceil(length(posts) / 4)
+    end
+  end
+
+  defp setup_posts(_) do
+    Application.put_env(:flop, :default_limit, 5)
+
+    publication =
+      publication_fixture(
+        name: "Blog",
+        description: "Blog description"
+      )
+
+    author =
+      author_fixture(publication_id: publication.id, name: "Author", bio: "Author description")
+
+    tag =
+      tag_fixture(publication_id: publication.id, name: "Tag", description: "Tag description")
+
+    posts =
+      for i <- 1..20 do
+        post_fixture(
+          title: "Post #{i}",
+          publication_id: publication.id,
+          authors_ids: [author.id],
+          tags_ids: [tag.id]
+        )
+      end
+
+    {:ok, binding()}
   end
 end
