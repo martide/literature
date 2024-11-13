aa86bf2c59df3b8410da691d3ad22d0aa01b97cc
Rommel Monsayac <rommelmonsayac@gmail.com>
2024-11-13 16:17:56 +0800

    Fix redirect form save button

--- lib/literature/components/form_component.ex
+++ lib/literature/components/form_component.ex
@@ -147,9 +147,11 @@ defmodule Literature.FormComponent do
     assigns =
       assigns
       |> assign_new(:disabled, fn -> false end)
+      |> assign_new(:form, fn -> nil end)
 
     ~H"""
     <button
+      form={@form}
       type="submit"
       class="w-full md:w-auto text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 md:mr-2 mb-2 focus:outline-none disabled:bg-opacity-70"
       disabled={@disabled}
