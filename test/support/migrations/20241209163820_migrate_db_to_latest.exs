538e7b28f5741780724f51b35ab2d27c9d45f02b
Rommel Monsayac <rommelmonsayac@gmail.com>
2025-01-07 22:29:14 +0800

    Remove concurrently

--- test/support/migrations/20241209163820_migrate_db_to_latest.exs
+++ test/support/migrations/20241209163820_migrate_db_to_latest.exs
@@ -1,9 +1,6 @@
 defmodule Literature.Test.Repo.Migrations.MigrateDbToLatest do
   use Ecto.Migration
 
-  @disable_ddl_transaction true
-  @disable_migration_lock true
-
   # Always migrate to the latest version for tests
   def up, do: Literature.Migrations.up([])
   def down, do: Literature.Migrations.down([])
