973d6aa70c1451e2c37fa5d73459f3b72ef35d3d
Sam Hamilton <sam@sh81.com>
2025-01-24 12:55:54 +0000

    Add lints (#389)
    * add lints
    
    * adjust cache
    
    * Fixed lint issue
    
    ---------
    
    Co-authored-by: dgross881 <dgross881@gmail.com>

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,29 +1,33 @@
-import EditorJS from '@editorjs/editorjs'
-import Header from '@editorjs/header'
-import Image from '@editorjs/image'
-import List from '@editorjs/list'
-import editorParser from 'editorjs-html'
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import Image from "@editorjs/image";
+import List from "@editorjs/list";
+import editorParser from "editorjs-html";
 
-const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+const csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
 
 // Custom image parser to put alt attribute
 const customImageParser = ({ data }) => {
-  const alt = data.alt ? data.alt : 'Image';
+  const alt = data.alt ? data.alt : "Image";
 
   return `<img src="${data.file && data.file.url ? data.file.url : data.url}" 
-    alt="${alt}" ${data.caption ? `caption="${data.caption}"` : ''} />`;
+    alt="${alt}" ${data.caption ? `caption="${data.caption}"` : ""} />`;
 };
 
 const parser = editorParser({ image: customImageParser });
 
-const HTMLEditorJS = element => {
-  const inputEditorJSON = document.querySelector('#form-editor-json')
-  const inputHTML = document.querySelector('#form-html')
+const HTMLEditorJS = (element) => {
+  const inputEditorJSON = document.querySelector("#form-editor-json");
+  const inputHTML = document.querySelector("#form-html");
 
-  inputEditorJSON.value = element.dataset.postData?
-    JSON.stringify(JSON.parse(element.dataset.postData)) : ""
-  inputHTML.value = element.dataset.postData ?
-    JSON.stringify(parser.parse(JSON.parse(element.dataset.postData))) : ""
+  inputEditorJSON.value = element.dataset.postData
+    ? JSON.stringify(JSON.parse(element.dataset.postData))
+    : "";
+  inputHTML.value = element.dataset.postData
+    ? JSON.stringify(parser.parse(JSON.parse(element.dataset.postData)))
+    : "";
 
   const editor = new EditorJS({
     tools: {
@@ -32,27 +36,27 @@ const HTMLEditorJS = element => {
         class: Image,
         config: {
           additionalRequestHeaders: {
-            'X-CSRF-TOKEN': csrfToken
+            "X-CSRF-TOKEN": csrfToken,
           },
           endpoints: {
             byFile: `${window.location.href}/upload-file`,
-            byURL: `${window.location.href}/fetch-url`
-          }
-        }
+            byURL: `${window.location.href}/fetch-url`,
+          },
+        },
       },
       list: {
         class: List,
-        inlineToolbar: true
+        inlineToolbar: true,
       },
     },
-    data: JSON.parse(element.dataset.postData || '{}'),
+    data: JSON.parse(element.dataset.postData || "{}"),
     onChange: () => {
       editor.save().then((outputData) => {
-        inputEditorJSON.value = JSON.stringify(outputData)
-        inputHTML.value = JSON.stringify(parser.parse(outputData))
-      })
-    }
-  })
-}
+        inputEditorJSON.value = JSON.stringify(outputData);
+        inputHTML.value = JSON.stringify(parser.parse(outputData));
+      });
+    },
+  });
+};
 
-export { HTMLEditorJS }
+export { HTMLEditorJS };
