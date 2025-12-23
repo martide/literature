08f31a7196293d05883440e9a9e026188f619592
Thanya Nitithatsanakul <ammbot@gmail.com>
2025-12-23 11:20:19 +0700

    Refactor image upload handling in PostController
    - Simplified the image upload logic using a `with` statement for better readability.
    - Updated error handling to return a 404 response when the post is not found, instead of a 400 response.
    - Added a test case to verify that a 404 response is returned when attempting to upload an image for a non-existent post.

--- lib/literature/controllers/post_controller.ex
+++ lib/literature/controllers/post_controller.ex
@@ -2,27 +2,29 @@ defmodule Literature.PostController do
   use Literature.Web, :controller
 
   def upload_image(conn, %{"image" => image} = params) do
-    params
-    |> get_post()
-    |> Literature.update_post(%{"upload_image" => rename_filename(image)})
-    |> case do
-      {:ok, post} ->
-        conn
-        |> put_resp_content_type("application/json")
-        |> send_resp(
-          200,
-          Jason.encode!(%{
-            success: 1,
-            file: %{
-              url: literature_image_url(post, :upload_image)
-            }
-          })
-        )
-
+    with %Literature.Post{} = post <- get_post(params),
+         {:ok, post} <- Literature.update_post(post, %{"upload_image" => rename_filename(image)}) do
+      conn
+      |> put_resp_content_type("application/json")
+      |> send_resp(
+        200,
+        Jason.encode!(%{
+          success: 1,
+          file: %{
+            url: literature_image_url(post, :upload_image)
+          }
+        })
+      )
+    else
       {:error, %Ecto.Changeset{}} ->
         conn
         |> put_resp_content_type("application/json")
         |> send_resp(400, Jason.encode!(%{success: 0}))
+
+      nil ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(404, Jason.encode!(%{success: 0, error: "Post not found"}))
     end
   end
 
